cdb433e5463a22348b3ea6994682571139cbbce1
karan seangar <seangarKaran@gmail.com>
2025-01-17 14:03:58 +0530

    multiplication file added to multiplication branch

---
+++ multiplication.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class multiplication {
+     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter number 1");
+        double num1 = sc.nextInt();
+        System.out.println("Enter number 2");
+        double num2 = sc.nextInt();
+        System.out.println(num1*num2);
+    }
+}
