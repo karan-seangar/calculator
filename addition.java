3c53ecefc72ccb629d6115960e00011e0288e9d9
karan seangar <seangarKaran@gmail.com>
2025-01-17 13:46:51 +0530

    addition commit

---
+++ addition.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+class addition{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter number 1");
+        double num1 = sc.nextInt();
+        System.out.println("Enter number 2");
+        double num2 = sc.nextInt();
+        System.out.println("Enter number 2");
+        double num3 = sc.nextInt();
+        System.out.println(num1+num2+num3);
+
+    }
+}
